50ba7503c7011910e45c4a09f4ca7ab8fca22469
a.mohamed_at_areeb <alaa.mohamed@areebgroup.com>
2020-05-31 22:45:45 +0200

    First initialize

---
+++ main.js
@@ -0,0 +1,29 @@
+/*
+Closure from MDN
+
+A closure is the combination of a function bundled together (enclosed)
+with references to its surrounding state (the lexical environment).
+In other words, a closure gives you access to an outer function’s scope
+from an inner function. In JavaScript, closures are created every time
+a function is created, at function creation time.
+*/
+
+/*
+Closure from my understanding as 
+
+However outer function is executed and inner function is defined within
+that scope, inner function is able to hold on to the reference of
+outer scope (outer function), and whatever you pass to the
+function (outer function), it continues to have access to it.
+inner function have a linkage to the outer scope or whatever
+a variable is defined in it.
+*/
+
+function outer (x) {
+    function inner() {
+       console.log(x)
+    }
+    inner()
+}
+
+outer(1) // 1
